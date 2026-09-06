0015477a6b3a38828d330f1696571ce2cb2ffcdb
jspg459-code <jspg459@gmail.com>
2026-09-07 00:22:43 +0200

    Show language selector globally

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import LanguageSelector from "./components/LanguageSelector";
 
 export const metadata = {
   title: "AdPoints",
@@ -6,5 +7,5 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return <html lang="fr"><body>{children}</body></html>;
+  return <html lang="fr"><body>{children}<LanguageSelector /></body></html>;
 }
