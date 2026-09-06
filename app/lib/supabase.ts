6bcd6d206272eb745aacb7a0b5830ba2d9d8f62c
jspg459-code <jspg459@gmail.com>
2026-09-06 22:27:47 +0200

    Fix Supabase client key

--- app/lib/supabase.ts
+++ app/lib/supabase.ts
@@ -2,5 +2,5 @@ import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
   "https://ftfvtdioloaymxfaznst.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXAiLCJyZWYiOiJmdGZ2dGRpb2xvYXlt eGZhem5zdCIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNzQ0NzM2ODgzLCJleHAiOjIwNjAzMTI4ODB9.tWNqiDrFrMLYzR8kNWAhsuUlcO28NEustf2pjip3P1U".replace(" ","")
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ0ZnZ0ZGlvbG9heW14ZmF6bnN0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ3MzY4ODMsImV4cCI6MjA2MDMxMjg4M30.tWNqiDrFrMLYzR8kNWAhsuUlcO28NEustf2pjip3P1U"
 );
