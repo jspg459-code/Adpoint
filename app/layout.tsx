2dbbc607080faeead50816f3dc2b23a99ca4f2c6
jspg459-code <jspg459@gmail.com>
2026-09-07 11:57:27 +0200

    Supprime l’ancien système de badge flottant

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import "./globals.css";
 import LanguageSelector from "./components/LanguageSelector";
-import ViewportControlsTopLayer from "./components/ViewportControlsTopLayer";
 
 export const metadata = {
   title: "AdPoints",
@@ -12,15 +11,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="fr">
       <body>
         {children}
-
-        <div className="globalBetaBadge" aria-label="Version bêta" data-no-translate>
-          BÊTA TEST
-        </div>
-
         <LanguageSelector />
-
-        {/* Promotes both controls into the browser Top Layer. */}
-        <ViewportControlsTopLayer />
       </body>
     </html>
   );
