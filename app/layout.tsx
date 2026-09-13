0600ca99896379c6f009534b89be620886e70e54
jspg459-code <jspg459@gmail.com>
2026-09-13 12:33:56 +0200

    Add Google AdSense verification code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Script from "next/script";
 import LanguageSelector from "./components/LanguageSelector";
 import BetaBadge from "./components/BetaBadge";
 import ActivityLogger from "./components/ActivityLogger";
@@ -15,6 +16,14 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="fr">
+      <head>
+        <Script
+          async
+          strategy="afterInteractive"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8585188868825763"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body>
         <PointsProvider>
           {children}
